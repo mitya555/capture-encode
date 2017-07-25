1d48011419afeb63e44d4976f1edb286a9dfaea6
Dmitriy.Mukhin@gmail.com <dmitriy.mukhin@gmail.com>
2017-07-25 14:33:49 -0400

    10/5/2015 change

--- raspivid-rtmp.sh
+++ raspivid-rtmp.sh
@@ -1,2 +1,2 @@
 set -x
-raspivid -n -mm matrix -w 1280 -h 720 -fps 25 -g 50 -t 0 -b 500000 -o - | ffmpeg -f h264 -r 25 -i - -c:v copy -an -f flv -rtmp_buffer 100 -rtmp_live live rtmp://localhost/rtmp/webcam
+raspivid -n -mm matrix -w 1280 -h 720 -fps 25 -g 50 -t 0 -b 2000000 -o - | ffmpeg -f h264 -r 25 -i - -c:v copy -an -f flv -rtmp_buffer 100 -rtmp_live live rtmp://10.44.34.225/rtmp/webcam
--- rtmp.sh
+++ rtmp.sh
@@ -1,2 +1,2 @@
 set -x
-./capture-encode -nb 300000 | ffmpeg -f h264 -r 25 -i - -c:v copy -an -f flv -rtmp_buffer 100 -rtmp_live live rtmp://localhost/rtmp/webcam
+./capture-encode -nb 300000 | ffmpeg -f h264 -r 25 -i - -c:v copy -an -f flv -rtmp_buffer 100 -rtmp_live live rtmp://10.44.34.225/rtmp/webcam
